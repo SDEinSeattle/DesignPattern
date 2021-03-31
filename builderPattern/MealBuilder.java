ae34a278ba8b535da094cb3652fc7d33c6950be4
Feng <80781077+SDEinSeattle@users.noreply.github.com>
2021-03-31 11:42:02 -0700

    Add files via upload

---
+++ builderPattern/MealBuilder.java
@@ -0,0 +1,10 @@
+package DesignPattern.BuilderPattern;
+
+public class MealBuilder {
+    public Meal prepareVegMeal() {
+        Meal meal = new Meal();
+        meal.addItem(new VegBurger());
+        meal.addItem(new Coke());
+        return meal;
+    }
+}
